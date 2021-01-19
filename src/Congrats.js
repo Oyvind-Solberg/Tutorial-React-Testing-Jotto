243a2aa4d765b9138421df05d090cb724d8ead4f
oyvindsol <oyvindsolberg.pro@gmail.com>
2021-01-19 14:40:37 +0100

    Add proptypes for Congrats component

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
  * Functional react component for congratulatory message.
@@ -7,7 +8,7 @@ import React from 'react';
  * @returns {JSX.Element} - Render component (or null if 'success' prop is false).
  */
 
-export default (props) => {
+const Congrats = (props) => {
 	if (props.success) {
 		return (
 			<div data-test="component-congrats">
@@ -18,3 +19,9 @@ export default (props) => {
 		);
 	} else return <div data-test="component-congrats"></div>;
 };
+
+Congrats.propTypes = {
+	success: PropTypes.bool.isRequired,
+};
+
+export default Congrats;
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Congrats from './Congrats';
+import checkPropTypes from 'check-prop-types';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const defaultProps = { success: false };
 
 /**
  * Factory function to create a ShallowWrapper for the Congrats component.
@@ -15,7 +18,8 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  */
 
 const setup = (props = {}) => {
-	return shallow(<Congrats {...props} />);
+	const SetupProps = { ...defaultProps, ...props };
+	return shallow(<Congrats {...SetupProps} />);
 };
 
 test('renders without error', () => {
@@ -34,4 +38,9 @@ test('renders non-empty congrats message when success prop is true', () => {
 	const wrapper = setup({ success: true });
 	const message = findByTestAttr(wrapper, 'congrats-message');
 	expect(message.text().length).not.toBe(0);
+});
+
+test('does not throw warning with expected props', () => {
+	const expectedProps = { success: false };
+	checkProps(Congrats, expectedProps);
 });
