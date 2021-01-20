3d019471d16149da5c9dae9d47c68cfaebc5a135
oyvindsol <oyvindsolberg.pro@gmail.com>
2021-01-20 15:38:53 +0100

    Start tests for GuessedWords

--- test/testUtils.js
+++ test/testUtils.js
@@ -2,6 +2,7 @@ import checkPropTypes from 'check-prop-types';
 
 /**
  * Return node(s) with the given data-test attribute.
+ * @function findByTestAttr
  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
  * @param {string} val - Value of the data-test attribute for search.
  * @return {ShallowWrapper}
