1a5e23cbaf5ec7e819373479f5bb694eb5e71a67
oyvindsol <oyvindsolberg.pro@gmail.com>
2021-01-27 14:12:18 +0100

    Add basic app component

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from '../test/testUtils';
 import App from './App';
 
-test('testing', () => {});
+/**
+ * Factory function to create a ShallowWrapper for the App component.
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (props = {}) => {
+	return shallow(<App />);
+};
+
+test('App renders without error', () => {
+	const wrapper = setup();
+	const component = findByTestAttr(wrapper, 'component-app');
+	expect(component.length).toBe(1);
+});
