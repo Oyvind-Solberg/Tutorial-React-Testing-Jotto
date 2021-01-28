a743342620ae94fd423b27b73a81a09cab5040b7
oyvindsol <oyvindsolberg.pro@gmail.com>
2021-01-28 14:34:03 +0100

    Add Input state-controled field

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 import Input from './Input';
+
+const defaultProps = {
+	secretWord: 'party',
+};
 
 /**
  * Factory function to create a ShallowWrapper for the Input component.
  * @function setup
+ * @param {object} props - Component props spesific to this setup
  * @returns {ShallowWrapper}
  */
 
 const setup = (props = {}) => {
-	return shallow(<Input />);
+	const setupProps = { ...defaultProps, ...props };
+	return shallow(<Input {...setupProps} />);
 };
 
 test('Input renders without error', () => {
 	const wrapper = setup();
 	const component = findByTestAttr(wrapper, 'component-input');
 	expect(component.length).toBe(1);
+});
+
+test('does not throw warning with expected props', () => {
+	checkProps(Input, defaultProps);
+});
+
+describe('state controlled input field', () => {
+	test('state updates with value of input box upon change', () => {
+		const mockSetCurrentGuess = jest.fn();
+		React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+		const wrapper = setup();
+		const inputBox = findByTestAttr(wrapper, 'input-box');
+
+		const mockEvent = { target: { value: 'train' } };
+		inputBox.simulate('change', mockEvent);
+
+		expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+	});
 });
